909009b1d8ebc7487724e6e0c9ec8e7be7a021c8
Tomas Andrek <tomas.andrek@xolution.sk>
2020-03-22 00:24:16 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# centralregister-mockserver
-This is a mock server for currently being prepared Energy Central Register for the UK. Written in Python using connexion and Flask
+# Central Register Mock Server
+This is a mock server for currently being prepared Energy Central Register for the UK. Written in Python using connexion and Flask.
+How to run it:
+> 1. Install all necessary packages: **pip install -r requirements.txt**
+> 2. Run **app.py**
---
+++ mockedData.py
@@ -0,0 +1,38 @@
+def schemes__get() -> str:
+    return 'schemes__get'
+
+def schemes__post() -> str:
+    return 'schemes__post'
+
+def schemes_x_assessors__get(schemeId) -> str:
+    return 'schemes_x_assessors__get schemeId={schemeId}'.format(schemeId=schemeId)
+
+def schemes_x_assessors_x__get(schemeId, schemeAssessorId) -> str:
+    return 'schemes_x_assessors_x__get schemeId={schemeId} schemeAssessorId={schemeAssessorId}'.format(schemeId=schemeId, schemeAssessorId=schemeAssessorId)
+
+def schemes_x_assessors_x__put(schemeId, schemeAssessorId) -> str:
+    return 'schemes_x_assessors_x__put schemeId={schemeId} schemeAssessorId={schemeAssessorId}'.format(schemeId=schemeId, schemeAssessorId=schemeAssessorId)
+
+def search_assessors__get() -> str:
+    return 'search_assessors__get'
+
+def search_addresses__get() -> str:
+    return 'search_addresses__get'
+
+def assessments_x__get() -> str:
+    return 'assessments_x__get'
+
+def assessments_x_x__post(assessmentType, assessmentId) -> str:
+    return 'assessments_x_x__post assessmentType={assessmentType} assessmentId={assessmentId}'.format(assessmentType=assessmentType, assessmentId=assessmentId)
+
+def reports_assessors_status__get() -> str:
+    return 'reports_assessors_status__get'
+
+def migrations_assessors_x__put(schemeAssessorId) -> str:
+    return 'schemes_x_assessors_x__put  schemeAssessorId={schemeAssessorId}'.format(schemeAssessorId=schemeAssessorId)
+
+def migrations_assessments_x__put(assessmentId) -> str:
+    return 'migrations_assessments_x__put  assessmentId={assessmentId}'.format(assessmentId=assessmentId)
+
+def migrations_addresses_x__put(addressId) -> str:
+    return 'migrations_addresses_x__put  addressId={addressId}'.format(addressId=addressId)
