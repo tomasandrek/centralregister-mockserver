a6d47fa56b9c8b95fe08e8510987c81569792eb4
Vlado Polacok <vladimir.polacok@elmhurstenergy.co.uk>
2020-04-15 17:05:22 +0200

    Amended mock data response for GET addresses

--- mockedData.py
+++ mockedData.py
@@ -24,43 +24,43 @@ addresses.append(address("PREVIOUS_CERTIFICATE", "2 Marsham Street", "Line2",
 addresses.append(address("PREVIOUS_CERTIFICATE", "10 Marsham Street",
                          "", "", "", "London", "SW1P 4JA", "RRN-8290-6027-4450-1230-5296", []))
 
-addresses.append(address("GAZETTEER", "16 St. Johns Business Park", "",
-                         "Leicetershure", "", "LUTTERWORTH", "LE17 4HB", "UPRN-5498652", []))
-addresses.append(address("GAZETTEER", "The Cottage, Church Lane", "Comberton",
-                         "", "", "CAMBRIDGE", "CB23 7ED", "UPRN-54112352", []))
-addresses.append(address("GAZETTEER", "Ladywood Works, Leicester Road", "",
-                         "Leicestershire", "", "LUTTERWORTH", "LE17 4HD", "UPRN-12312352", []))
-addresses.append(address("GAZETTEER", "Flat 5 Laughton Court, Stoughton Road", "",
+addresses.append(address("GAZETTEER", "Elmhurst Energy Systems Ltd", "16 St. Johns Business Park",
+                         "", "", "LUTTERWORTH", "LE17 4HB", "UPRN-5498652", []))
+addresses.append(address("GAZETTEER", "The Cottage", "Church Lane",
+                         "Comberton", "", "CAMBRIDGE", "CB23 7ED", "UPRN-54112352", []))
+addresses.append(address("GAZETTEER", "H W Coates Ltd", "Ladywood Works, Leicester Road",
+                         "", "", "LUTTERWORTH", "LE17 4HD", "UPRN-12312352", []))
+addresses.append(address("GAZETTEER", "Flat 5 Laughton Court", "Stoughton Road",
                          "", "", "LEICESTER", "LE2 2ED", "UPRN-9872352", []))
-addresses.append(address("GAZETTEER", "56 Wendover Heights, Old Tring Road", "Wendover",
-                         "Buckinghamshire", "", "AYLESBURY", "HP22 6PH", "UPRN-88009872352", []))
-addresses.append(address("GAZETTEER", "Flat 2, 1, Upper Cape", "",
+addresses.append(address("GAZETTEER", "56, Wendover Heights", "Old Tring Road",
+                         "", "", "AYLESBURY", "HP22 6PH", "UPRN-88009872352", []))
+addresses.append(address("GAZETTEER", "Flat 2", "1, Upper Cape",
                          "", "", "WARWICK", "CV34 5DS", "UPRN-14009872352", []))
 
 addresses.append(address("GAZETTEER", "The Vestry", "Llanddewi Velfrey",
-                         "Dyfed", "", "NARBERTH", "SA67 7EG", "UPRN-14009871232", []))
+                         "", "", "NARBERTH", "SA67 7EG", "UPRN-14009871232", []))
 addresses.append(address("GAZETTEER", "Bryn Mor", "Rhosgadfan",
-                         "Gwynedd", "", "CAERNARFON", "LL54 7HR", "UPRN-98709871232", []))
+                         "", "", "CAERNARFON", "LL54 7HR", "UPRN-98709871232", []))
 addresses.append(address("GAZETTEER", "10, Lon Ty'n-y-Cae", "",
                          "", "", "CARDIFF", "CF14 6DD", "UPRN-98882871232", []))
 addresses.append(address("GAZETTEER", "59, Hanbury Road", "Pontnewynydd",
-                         "Gwent", "", "PONTYPOOL", "NP4 6PF", "UPRN-11882871232", []))
+                         "", "", "PONTYPOOL", "NP4 6PF", "UPRN-11882871232", []))
 addresses.append(address("GAZETTEER", "Cae'r Odyn", "Rhiw",
-                         "Gwynedd", "", "PWLLHELI", "LL53 8AS", "UPRN-11882221232", []))
+                         "", "", "PWLLHELI", "LL53 8AS", "UPRN-11882221232", []))
 addresses.append(address("GAZETTEER", "28, Penrho Estate", "Mostyn",
-                         "Clwyd", "", "HOLYWELL", "CH8 9QS", "UPRN-18882221232", []))
+                         "", "", "HOLYWELL", "CH8 9QS", "UPRN-18882221232", []))
 
-addresses.append(address("GAZETTEER", "Metropolitan Building 29-31, Alfred Street", "",
+addresses.append(address("GAZETTEER", "Atos Healthcare", "Metropolitan Building 29-31, Alfred Street",
                          "", "", "BELFAST", "BT2 8ED", "UPRN-17742221232", []))
-addresses.append(address("GAZETTEER", "Gordon House, 22-24, Lombard Street", "",
+addresses.append(address("GAZETTEER", "Belfast Health Cities", "Gordon House, 22-24, Lombard Street",
                          "", "", "BELFAST", "BT1 1RD", "UPRN-47742221232", []))
-addresses.append(address("PREVIOUS_CERTIFICATE", "Gordon House, 22-24, Lombard Street", "",
-                         "", "", "BELFAST", "BT1 1RD", "RRN-1110-6027-4450-1230-5296", []))
+addresses.append(address("PREVIOUS_CERTIFICATE", "17a, Moss Road", "Ballinaskeagh",
+                         "", "", "BANBRIDGE", "BT32 5EF", "RRN-1110-6027-4450-1230-5296", []))
 addresses.append(address("GAZETTEER", "78, St. James's Road", "",
                          "", "", "BELFAST", "BT12 6ED", "UPRN-88742110232", []))
 addresses.append(address("GAZETTEER", "39, Lord Warden's Glade", "",
-                         "County Down", "", "BANGOR", "BT19 1GW", "UPRN-98742110232", []))
-addresses.append(address("GAZETTEER", "Apartment 219 St. Anne's Square, 10, Edward Street", "",
+                         "", "", "BANGOR", "BT19 1GW", "UPRN-98742110232", []))
+addresses.append(address("GAZETTEER", "Apartment 219", "10, Edward Street",
                          "", "", "BELFAST", "BT1 2LP", "UPRN-14742110232", []))
 
 
